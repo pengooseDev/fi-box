64414cadb9757e5d1603e410b90783271f736c0b
dkedid <73521518+pengoosedev@users.noreply.github.com>
2022-07-13 22:43:19 +0900

    updateSoundFunc

--- src/App.tsx
+++ src/App.tsx
@@ -127,7 +127,7 @@ const FileBoard = styled.ul`
         background: rgba(0, 0, 0, 0.2);
     }
     ::-webkit-scrollbar-thumb {
-        background: rgba(232, 19, 169, 0.65);
+        background: rgba(232, 19, 169, 0.75);
         border-radius: 5px 0px 0px 5px;
         box-shadow: 0px 0px 5px black;
     }
@@ -168,11 +168,14 @@ const PlayerBoard = styled.div<IPlayerQueueLength>`
 
     z-index: 3;
     //Default
-    /*Droppable일때 RBD는 board의 display가 아래로만 확장한다는 한계가 있음.
+
+    /*
+    Droppable일때 RBD는 board의 display가 아래로만 확장한다는 한계가 있음.
     Hover로 top의 position을 처리할 경우, Draggable의 어디를 클릭하느냐에 따라 유의미한 오차가 발생.
     따라서, props에서 draggingOver일 때, top의 position이 변하도록 구현. 
     react-dnd쓰자..
     */
+
     top: ${(props) =>
         props.queueLength
             ? props.queueLength > 3
@@ -204,14 +207,17 @@ const PlayerBoard = styled.div<IPlayerQueueLength>`
     ::-webkit-scrollbar {
         width: 12px;
     }
+
     ::-webkit-scrollbar-track {
         background: rgba(0, 0, 0, 0.2);
     }
+
     ::-webkit-scrollbar-thumb {
-        background: rgba(232, 19, 169, 0.65);
-        border-radius: 5px 0px 0px 5px;
+        background: rgba(232, 19, 169, 0.75);
+        border-radius: 5px;
         box-shadow: 0px 0px 5px black;
     }
+
     background: rgba(255, 255, 255, 0.3);
     border-radius: 5px 0px 0px 5px;
     transition: 0.09s ease-in-out;
--- src/components/soundFunc.tsx
+++ src/components/soundFunc.tsx
@@ -13,26 +13,60 @@ import mel7SFX from "../assets/audio/lp/mel7.mp3";
 2. queue 시작시, recoil에서 data를 받아와서 array로 재생.
 */
 
+const queueObject = {
+    //앞부분이 recoil에서 받아오는 LP이름
+    LP1: new Audio(mel1SFX),
+    LP2: new Audio(mel2SFX),
+    LP3: new Audio(mel3SFX),
+    LP4: new Audio(mel4SFX),
+    LP5: new Audio(mel5SFX),
+    LP6: new Audio(mel6SFX),
+    LP7: new Audio(mel7SFX),
+};
+
+const soundHandler = async () => {
+    const localData = localStorage.getItem("recoil-persist");
+    if (!localData) return;
+    const parsedLocalData = JSON.parse(localData);
+    const queue = parsedLocalData["lpState"]["player"];
+    console.log(queue);
+
+    const promiseArray: void[] = [];
+
+    //@ts-ignore
+    queue.map((i: string) => promiseArray.push(queueObject[`${i}`]));
+
+    //@ts-ignore
+    Promise.all(promiseArray.map((i) => console.log(i)));
+
+    //@ts-ignore
+    Promise.all(promiseArray.map((i) => i.play()));
+};
+
+/////////
+/////////
+/////////
+/*
 const mel1 = () => {
-    return new Audio(mel1SFX).play();
+    new Audio(mel1SFX).play();
 };
 const mel2 = () => {
-    return new Audio(mel2SFX).play();
+    new Audio(mel2SFX).play();
 };
 const mel3 = () => {
-    return new Audio(mel3SFX).play();
+    new Audio(mel3SFX).play();
 };
 const mel4 = () => {
-    return new Audio(mel4SFX).play();
+    new Audio(mel4SFX).play();
 };
 const mel5 = () => {
-    return new Audio(mel5SFX).play();
+    new Audio(mel5SFX).play();
 };
 const mel6 = () => {
-    return new Audio(mel6SFX).play();
+    new Audio(mel6SFX).play();
 };
 const mel7 = () => {
-    return new Audio(mel7SFX).play();
+    new Audio(mel7SFX).play();
 };
 
 const queueObject = {
@@ -45,6 +79,7 @@ const queueObject = {
     LP6: mel6,
     LP7: mel7,
 };
+
 
 const soundHandler = async () => {
     const localData = localStorage.getItem("recoil-persist");
@@ -61,11 +96,15 @@ const soundHandler = async () => {
     //@ts-ignore
     Promise.all(promiseArray.map((i) => i()));
 };
+*/
+////////////////
+////////////////
+////////////////
 
 const SoundBox = () => {
     //90BPM 4var 10.66s
     React.useEffect(() => {
-        setInterval(soundHandler, 11296);
+        setInterval(soundHandler, 11406);
     }, []);
 
     //85BPM(16bit) => 11,296ms
